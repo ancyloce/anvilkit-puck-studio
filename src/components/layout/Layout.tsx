70cda2c580974f69aa26cea0b06f0f138948234b
ancyloce <ancyloce@gmail.com>
2026-03-13 01:31:25 -0700

    🔧 refactor: Improve layout structure in EditorLayout component by adjusting div hierarchy and ensuring consistent overflow handling for better responsiveness and usability.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -15,26 +15,24 @@ export const EditorLayout = ({ aiPanel }: EditorLayoutProps) => {
     const activeTab = useStore(uiStore, (s) => s.activeTab);
 
     return (
-        <div className="max-sm:hidden overflow-hidden h-screen">
-            <Header />
-            <motion.div
-                className="relative w-full flex border-r border-neutral-200 dark:border-neutral-800/80 bg-white/60 dark:bg-neutral-950/80 backdrop-blur-md ml-0 h-[calc(100vh-54px)]"
-            >
-                <Aside></Aside>
-                <div className="relative h-full flex flex-col overflow-x-hidden overflow-y-auto border-r border-neutral-200 dark:border-neutral-800/80 w-[240px]">
-                    {activeTab === "insert" && <Puck.Components />}
-                    {activeTab === "layer" && <Puck.Outline />}
-                    {activeTab === "copilot" && aiPanel}
-                </div>
-                <div className="flex-1 h-full">
-                    <ScrollArea className="h-full">
-                        <Puck.Preview></Puck.Preview>
-                    </ScrollArea>
-                </div>
-                <div className="border-l border-neutral-200 dark:border-neutral-800/80">
-                    <Puck.Fields></Puck.Fields>
-                </div>
-            </motion.div>
-        </div>
+      <div className="max-sm:hidden overflow-hidden h-screen">
+        <Header />
+        <motion.div className="relative w-full flex border-r border-neutral-200 dark:border-neutral-800/80 bg-white/60 dark:bg-neutral-950/80 backdrop-blur-md ml-0 h-[calc(100vh-54px)]">
+          <Aside></Aside>
+          <div className="relative h-full flex flex-col overflow-x-hidden overflow-y-auto border-r border-neutral-200 dark:border-neutral-800/80 w-[240px]">
+            {activeTab === "insert" && <Puck.Components />}
+            {activeTab === "layer" && <Puck.Outline />}
+            {activeTab === "copilot" && aiPanel}
+          </div>
+          <div className="flex-1 h-full overflow-hidden">
+            <ScrollArea className="h-full">
+              <Puck.Preview></Puck.Preview>
+            </ScrollArea>
+          </div>
+          <div className="border-l border-neutral-200 dark:border-neutral-800/80  w-[240px]">
+            <Puck.Fields></Puck.Fields>
+          </div>
+        </motion.div>
+      </div>
     );
 };
